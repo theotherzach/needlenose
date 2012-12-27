26d58d9b20e8ea8ee0ed9ae473fdbd3bf478e6d2
TheOtherZach <briggszj@gmail.com>
2012-12-26 19:22:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Needlenose::Application.config.secret_token = '2bbd90420edd7497a5f7d25cc14e9dd214b809052925db380f0d7fecef3ee931f8378c31ac91c58e59902877814f669a6bcaa00d074f5c209e124f5f3034855a'
